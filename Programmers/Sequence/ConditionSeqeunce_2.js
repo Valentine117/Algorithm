950134208a908bf688e25b58f7dd5aae839c385e
Valentine117 <shmidas117@naver.com>
2023-12-08 19:13:54 +0900

    feat: add seq 2 file

--- Programmers/Sequence/ConditionSeqeunce_2.js
+++ Programmers/Sequence/ConditionSeqeunce_2.js
@@ -1,4 +1,16 @@
-function solution(arr) {
-  var answer = 0;
-  return answer;
+function recur(arr, n) {
+  let newArr = arr.map(it => {
+    if(it>=50 && it%2===0) return it/2;
+    else if(it<50 && it%2!==0) return it*2 + 1;
+    else return it;
+  })
+
+  if(arr.toString() === newArr.toString()) return n;
+  return recur(newArr, n+1);
 }
+
+function solution(arr) {
+  return recur(arr,0);
+}
+
+console.log(solution([1, 2, 3, 100, 99, 98], 5))

4e08949cc0f5583514b6beb9aa082d2939a7a690
Valentine117 <shmidas117@naver.com>
2023-12-14 20:01:51 +0900

    feat: complete sketch prob

--- Programmers/Normal/SketchyKeypad.js
+++ Programmers/Normal/SketchyKeypad.js
@@ -1,6 +1,17 @@
 function solution(keymap, targets) {
   var answer = [];
-  return answer;
+
+  const map = {};
+  keymap.forEach(it => {
+    it.split("").forEach((val, idx) => {
+      if(!map[val]) {
+      map[val] = idx + 1;
+    } else if (map[val] > idx + 1) map[val] = idx + 1;})
+  });
+
+  console.log(map)
+
+  return targets.map(it=> it.split('').reduce((acc, val) => acc + map[val], 0) ?? null);
 }
 
 console.log(solution(["ABACD", "BCEFD"],	["ABCD","AABB"]))

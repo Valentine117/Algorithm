ebc88ba1e322e8a9376dd71bac48cf69f2209244
Valentine117 <shmidas117@naver.com>
2023-12-14 21:42:48 +0900

    feat: complete parkWalk

--- Programmers/Normal/ParkWalk.js
+++ Programmers/Normal/ParkWalk.js
@@ -1,13 +1,6 @@
 function solution(park, routes) {
-  const oldGraph = park.map(it => it.split(""));
+  const graph = park.map(it => it.split(""));
   const start = [0,0];
-  const graph = Array(oldGraph[0].length).fill(0).map(it => Array(oldGraph.length).fill(0));
-
-  for (let i=0;i<oldGraph.length;i++) {
-    for (let j =0;j<oldGraph[0].length;j++) {
-      graph[j][i] = oldGraph[i][j];
-    }
-  }
 
   for (let i=0;i<graph.length;i++) {
     for (let j =0;j<graph[0].length;j++) {
@@ -19,30 +12,56 @@ function solution(park, routes) {
     }
   }
 
+
+  console.log(graph)
+
+  console.log(start)
+
   routes.map(it=>it.split(" ")).forEach(it=>{
+    console.log(it)
     switch (it[0]) {
-      case 'S':
-        for(let i=0;i<Number(it[1]);i++) {
-          if(start[1]+i > graph[0].length || graph[start[0]][start[1]+i] === 'X') break;
+      case 'E':
+        let outE = false;
+        for(let i=1;i<=Number(it[1]);i++) {
+          if(start[1]+i > graph[0].length-1 || graph[start[0]][start[1]+i] === 'X') {
+            outE = true;
+            break;
+          }
         }
+        if(outE) break
         start[1] += Number(it[1]);
         break;
-      case 'N':
-        for(let i=0;i<Number(it[1]);i++) {
-          if(start[1]-i < 0 || graph[start[0]][start[1]-i] === 'X') break;
+      case 'W':
+        let outW = false;
+        for(let i=1;i<=Number(it[1]);i++) {
+          if(start[1]-i < 0 || graph[start[0]][start[1]-i] === 'X') {
+            outW = true
+            break;
+          }
         }
+        if(outW) break;
         start[1] -= Number(it[1]);
         break;
-      case 'E':
-        for(let i=0;i<Number(it[1]);i++) {
-          if(start[0]+i > graph.length || graph[start[0]+i][start[1]] === 'X') break;
+      case 'S':
+        let outS = false;
+        for(let i=1;i<=Number(it[1]);i++) {
+          if(start[0]+i > graph[0].length-1 || graph[start[0]+i][start[1]] === 'X') {
+            outS = true;
+            break;
+          }
         }
+        if(outS) break;
         start[0] += Number(it[1]);
         break;
-      case 'W':
-        for(let i=0;i<Number(it[1]);i++) {
-          if(start[0]-i < 0 || graph[start[0]-i][start[1]] === 'X') break;
+      case 'N':
+        let outN = false;
+        for(let i=1;i<=Number(it[1]);i++) {
+          if(start[0]-i < 0 || graph[start[0]-i][start[1]] === 'X') {
+            outN = true;
+            break;
+          }
         }
+        if(outN) break;
         start[0] -= Number(it[1]);
         break;
     }

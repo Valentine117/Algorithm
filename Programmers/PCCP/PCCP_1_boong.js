5ead0f817d27c73b0963e2ce6ba126ab208d2f92
Valentine117 <shmidas117@naver.com>
2023-12-16 21:47:20 +0900

    feat: complete pccp 1 prob

--- Programmers/PCCP/PCCP_1_boong.js
+++ Programmers/PCCP/PCCP_1_boong.js
@@ -1,4 +1,30 @@
 function solution(bandage, health, attacks) {
-    var answer = 0;
-    return answer;
+    const firstHealth = health;
+    const lastIndex = attacks[attacks.length-1][0];
+    const attackIndexes = attacks.map(it=>it[0]);
+    const attackMap = {};
+    attacks.forEach(it => {attackMap[it[0]] = it[1]});
+
+    let stk = 0;
+
+    for(let i = 0; i<=lastIndex; i++) {
+        if(attackIndexes.includes(i)) {
+            health -= attackMap[i];
+            if(health <= 0) return -1;
+            stk = 0;
+            continue;
+        }
+        stk++;
+        health += bandage[1];
+        if(stk === bandage[0]) {
+            health += bandage[2];
+            stk = 0;
+        }
+
+        if(health > firstHealth) health = firstHealth;
+    }
+
+    return health;
 }
+
+console.log(solution([5, 1, 5], 30, [[2, 10], [9, 15], [10, 5], [11, 5]]))

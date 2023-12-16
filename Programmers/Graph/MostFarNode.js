15cc604ce0c11d046e5d4f2eaaa4ec4b19d8add1
Valentine117 <shmidas117@naver.com>
2023-12-17 00:22:19 +0900

    feat: complete the farthest node prob

--- Programmers/Graph/MostFarNode.js
+++ Programmers/Graph/MostFarNode.js
@@ -1,34 +1,33 @@
-
-var visited = Array(20001).fill(false);
-var distanceArr = Array(20001).fill(-1);
-function search(node, distance, graph) {
-  if(visited[node]) return;
-  visited[node] = true;
-  distanceArr[node] = distance;
-  console.log(graph)
-  for(const i of graph[node]) {
-    console.log(i);
-    search(i, distance + 1, [...graph]);
-  }
-}
-
 function solution(n, edge) {
-  let graph = Array(n+1).fill(1).map(it => []);
-  visited = visited.slice(0,n+1);
-  distanceArr = distanceArr.slice(0, n+1);
+  const visited = Array(n+1).fill(false);
+  const distanceArr = Array(n+1).fill(0);
 
-  for (const arr of edge) {
-    graph[arr[0]].push(arr[1]);
-    graph[arr[1]].push(arr[0]);
+  const graph = Array(n+1).fill(0).map(it=> []);
+
+  edge.forEach(val => {
+    graph[val[0]].push(val[1]);
+    graph[val[1]].push(val[0]);
+  })
+
+  console.log(graph)
+
+  const bfsList = [1];
+
+  while (bfsList.length > 0) {
+    const index = bfsList.shift();
+    visited[index] = true;
+    graph[index].forEach(it => {
+      if(!visited[it]) {
+        bfsList.push(it);
+        distanceArr[it] = distanceArr[index] + 1;
+        visited[it] = true;
+      }
+    })
   }
 
-  graph = graph.map(it => it.sort((a,b)=>a-b));
+  const maxNodeDistance = Math.max(...distanceArr);
 
-  console.log(graph);
-
-  search(1,0,graph);
-
-  return distanceArr;
+  return distanceArr.filter(it => it === maxNodeDistance).length;
 }
 
-console.log(solution(6, [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]	));
+console.log(solution(6, [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]));

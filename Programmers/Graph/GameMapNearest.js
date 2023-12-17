56e27fc589d9b2eed7c623c0e9b2f47b808fb2d4
Valentine117 <shmidas117@naver.com>
2023-12-17 19:37:59 +0900

    feat: complete gameMapNearest Prob

--- Programmers/Graph/GameMapNearest.js
+++ Programmers/Graph/GameMapNearest.js
@@ -1,22 +1,40 @@
-let LeastDistance = Infinity;
-
-function dfs(x, y, distance, maps, copyVisited) {
-    if(x<0 || y<0 || x>maps.length-1 || y>maps[0].length-1 || copyVisited[x][y] || maps[x][y] === 0) return;
-    copyVisited[x][y] = true;
-    if(x===maps.length-1 && y===maps[0].length-1) {
-        LeastDistance = Math.min(LeastDistance, distance);
-        return;
-    }
-    dfs(x,y+1, distance+1, maps, [...copyVisited]);
-    dfs(x+1,y, distance+1, maps, [...copyVisited]);
-    dfs(x,y-1, distance+1, maps, [...copyVisited]);
-    dfs(x-1,y, distance+1, maps, [...copyVisited]);
-}
-
 function solution(maps) {
     const visited = Array(maps.length).fill(0).map(_ => Array(maps[0].length).fill(false));
-    dfs(0,0,1, maps, visited)
-    return LeastDistance === Infinity ? -1 : LeastDistance;
+    const distanceArr = Array(maps.length).fill(0).map(_ => Array(maps[0].length).fill(0));
+    distanceArr[0][0] = 1;
+    const list = [[0,0]];
+
+    while (list.length > 0) {
+        const head = list.shift();
+        const x = head[0];
+        const y = head[1];
+
+        if(x === maps.length-1 && y === maps[0].length-1) return distanceArr[x][y];
+
+        visited[x][y] = true;
+
+        if(x + 1 < maps.length && maps[x+1][y] !== 0 && !visited[x+1][y]) {
+            distanceArr[x+1][y] = distanceArr[x][y] + 1;
+            list.push([x+1, y]);
+            maps[x+1][y] = 0;
+        }
+        if(y + 1 < maps[0].length && maps[x][y+1] !== 0 && !visited[x][y+1]) {
+            distanceArr[x][y+1] = distanceArr[x][y] + 1;
+            list.push([x, y+1]);
+            maps[x][y+1] = 0;
+        }
+        if(x - 1 >= 0 && maps[x-1][y] !== 0 && !visited[x-1][y]) {
+            distanceArr[x-1][y] = distanceArr[x][y] + 1;
+            list.push([x-1, y]);
+            maps[x-1][y] = 0;
+        }
+        if(y - 1 >= 0 && maps[x][y-1] !== 0 && !visited[x][y-1]) {
+            distanceArr[x][y-1] = distanceArr[x][y] + 1;
+            list.push([x, y-1]);
+            maps[x][y-1] = 0;
+        }
+    }
+    return -1;
 }
 
 console.log(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]));

ec78a017555a3e9445872b16cfbb01a40156c012
Valentine117 <shmidas117@naver.com>
2023-12-23 00:25:10 +0900

    feat: complete add pointing prob

--- Programmers/Hard/Pointing.js
+++ Programmers/Hard/Pointing.js
@@ -1,7 +1,7 @@
 function solution(k, d) {
     let answer = 0;
-    for(let i = 0; i <= d; i++) {
-        answer += Math.floor(Math.sqrt((d-i)*(d-i) - i*i));
+    for(let i = 0; i <= d; i+=k) {
+        answer += Math.floor(Math.floor(Math.sqrt(Math.abs(d*d - i*i)))/k)+1;
     }
     return answer;
 }

7a6da49128e3df3594240f2706f53df1456e4003
Valentine117 <shmidas117@naver.com>
2024-04-28 17:18:10 +0900

    feat: add lint and prettier file

---
+++ Programmers/Hard/EatingBottom.js
@@ -0,0 +1,15 @@
+function solution(land) {
+  let answer = 0;
+
+  for (let i = 1; i < land.length - 1; i++) {}
+
+  return answer;
+}
+
+console.log(
+  solution([
+    [1, 2, 3, 5],
+    [5, 6, 7, 8],
+    [4, 3, 2, 1],
+  ])
+);

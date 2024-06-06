c55418b52be2f4001a160a492cc0aacbf5c8c2f7
Valentine117 <shmidas117@naver.com>
2024-06-06 23:29:00 +0900

    WIP

--- Programmers/Hard/EatingBottom.js
+++ Programmers/Hard/EatingBottom.js
@@ -1,9 +1,11 @@
 function solution(land) {
-  let answer = 0;
-
-  for (let i = 1; i < land.length - 1; i++) {}
-
-  return answer;
+  for (let i = 1; i < land.length; i++) {
+    land[i][0] += Math.max(land[i - 1][1], land[i - 1][2], land[i - 1][3]);
+    land[i][1] += Math.max(land[i - 1][0], land[i - 1][2], land[i - 1][3]);
+    land[i][2] += Math.max(land[i - 1][0], land[i - 1][1], land[i - 1][3]);
+    land[i][3] += Math.max(land[i - 1][0], land[i - 1][1], land[i - 1][2]);
+  }
+  return Math.max(...land[land.length - 1]);
 }
 
 console.log(
@@ -11,5 +13,5 @@ console.log(
     [1, 2, 3, 5],
     [5, 6, 7, 8],
     [4, 3, 2, 1],
-  ])
+  ]),
 );

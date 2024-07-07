7b08e69ca833e2c9639062a450901eaecee99f00
Valentine117 <shmidas117@naver.com>
2024-07-07 11:20:34 +0900

    WIP

--- Programmers/BruteForceSearch/leastRect.js
+++ Programmers/BruteForceSearch/leastRect.js
@@ -1,5 +1,16 @@
 function solution(sizes) {
-  return Math.max(...sizes.map(it => Math.max(...it))) * Math.max(...sizes.map(it => Math.min(...it)));
+  return (
+    Math.max(...sizes.map((it) => Math.max(...it))) *
+    Math.max(...sizes.map((it) => Math.min(...it)))
+  );
 }
 
-console.log(solution([[60, 50], [30, 70], [60, 30], [80, 40]]));
+//TODO
+console.log(
+  solution([
+    [60, 50],
+    [30, 70],
+    [60, 30],
+    [80, 40],
+  ]),
+);

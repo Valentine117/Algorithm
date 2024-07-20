266bcd62763e325b99a09e638ff5e65fbdd51a53
Valentine117 <shmidas117@naver.com>
2024-07-20 23:36:27 +0900

    WIP

--- LeetCode/RomanToInt.js
+++ LeetCode/RomanToInt.js
@@ -13,8 +13,6 @@ var romanToInt = function (s) {
     M: 1000,
   };
 
-  //TODO
-
   return s.split('').reduce((acc, val, idx) => {
     if (idx + 1 < s.length)
       return dict[s.charAt(idx)] < dict[s.charAt(idx + 1)]
